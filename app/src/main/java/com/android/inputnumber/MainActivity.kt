066cdb28761fd079cd7b5c5d16600bc4b50edf13
plain-dev <28282306+plain-dev@users.noreply.github.com>
2021-11-04 23:39:08 +0800

    :hammer_and_wrench: Add sample code
    - Change the `Button` to `androidx.appcompat.widget.AppCompatTextView` (Better compatibility)
    - Adjust control style

--- app/src/main/java/com/android/inputnumber/MainActivity.kt
+++ app/src/main/java/com/android/inputnumber/MainActivity.kt
@@ -2,10 +2,39 @@ package com.android.inputnumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.inputnumber.lib.InputNumberView
+import com.android.inputnumber.lib.callback.OnInputNumberCallback
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var inputNumberView: InputNumberView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        inputNumberView = findViewById(R.id.inputNumberView)
+
+        initInputNumberView(inputNumberView)
     }
+
+    private fun initInputNumberView(inputNumberView: InputNumberView) {
+        with(inputNumberView) {
+            number = 10
+            minimum  = 2
+            maximum = 20
+            step = 2
+            onInputNumberCallback = object : OnInputNumberCallback {
+                override fun onUpdateNeedConfirmation(
+                    view: InputNumberView,
+                    number: Int,
+                    confirmBlock: () -> Unit,
+                    failedBlock: () -> Unit,
+                    obj: Any?
+                ) {
+                    confirmBlock()
+                }
+            }
+        }
+    }
+
 }
